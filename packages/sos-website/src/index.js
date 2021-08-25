7a919edfe401f1710c8c95229c1698c024acb515
bru170 <80203072+bru170@users.noreply.github.com>
2021-08-25 19:51:47 +0200

    set up frontity

---
+++ packages/sos-website/src/index.js
@@ -0,0 +1,19 @@
+const Root = () => {
+  return (
+    <>
+      <h1>Hello Frontity</h1>
+    </>
+  )
+}
+export default {
+  name: "sos-website",
+  roots: {
+    sos: Root
+  },
+  state: {
+    sos: {}
+  },
+  actions: {
+    sos: {}
+  }
+}
